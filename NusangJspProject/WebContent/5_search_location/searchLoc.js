44daa14ef7b59c6cbf93d80701dbdc940e540151
tmdghks7836 <tmdghks0021@gmail.com>
2020-10-25 17:25:40 +0900

    검색 js 수정

--- NusangJspProject/WebContent/5_search_location/searchLoc.js
+++ NusangJspProject/WebContent/5_search_location/searchLoc.js
@@ -1,6 +1,6 @@
 (function() {
 
-	/*$("#searchBtn").click(
+	$("#searchBtn").click(
 			function() {
 
 				$("#searchTable").empty();
@@ -41,9 +41,9 @@
 						alert(status);
 					}
 				});
-			});*/
+			});
 
-	/*function setLocation(locationName) {
+	function setLocation(locationName) {
 
 		let
 		modal = $('#modal_loc');
@@ -60,14 +60,12 @@
 			$.ajax({
 				url : "/user/set_location?searchValue=" + locationName,
 				success : function(data) {
-					
-					
-					//location.href = data;
+
+					// location.href = data;
 				}
 			})
-			
+
 			;
 		});
-	}
-	;*/
+	};
 })()
