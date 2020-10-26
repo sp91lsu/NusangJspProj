a9afe1505470ab6e6d9468becbf453828fdc46e6
tmdghks7836 <tmdghks0021@gmail.com>
2020-10-26 22:35:04 +0900

    no message

--- NusangJspProject/src/com/nusang/dao/LocationDao.java
+++ NusangJspProject/src/com/nusang/dao/LocationDao.java
@@ -26,25 +26,33 @@ public class LocationDao extends BasicDao<Location> {
 	}
 
 	private LocationDao(String namespace) {
-		super(namespace,"locationno");
+		super(namespace, "locationno");
 	}
 
-	public ArrayList<Post> findByLocation(Location location)
-	{
+	public ArrayList<Post> findByLocation(Location location) {
 		SqlSession session = sqlSessionFactory.openSession();
-		Map<String, Object> map = new HashMap<String, Object>();
-		map.put("name1", location.getName1());
-		map.put("name2", location.getName2());
-		map.put("name3", location.getName3());
-		//유저의 동네 리스트를  다 가지고 온다. 
-		List<Location> locationList = session.selectList(namespace + "findByLocationName", map);
-		
-	//	List<Post>
-		for (Location entity : locationList) {
-			PostDao.getInstance().findBy("locationno", location.getLocationno());
+		ArrayList<Post> postList = new ArrayList<Post>();
+		try {
+
+			Map<String, Object> map = new HashMap<String, Object>();
+			map.put("name1", location.getName1());
+			map.put("name2", location.getName2());
+			map.put("name3", location.getName3());
+			// 유저의 동네 리스트를 다 가지고 온다.
+			List<Location> locationList = session.selectList(namespace + "findByLocationName", map);
+
+			for (Location entity : locationList) {
+				Post post = PostDao.getInstance().findBy(session, "locationno", entity.getLocationno());
+				postList.add(post);
+			}
+			session.commit();
+
+		} catch (Exception e) {
+			session.rollback();
+			e.printStackTrace();
+		} finally {
+			session.close();
 		}
-		session.commit();
-		session.close();
-		return null;
+		return postList;
 	}
 }
