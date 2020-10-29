198b0e9f2ce77d4d78b217e0fc072c17490c66ba
cheonui <zzangkyo10@gmail.com>
2020-10-29 16:59:22 +0900

    conassist에 myinfoDistance 서블릿경로 추가

--- NusangJspProject/src/com/nusang/controller/assistance/ConAsist.java
+++ NusangJspProject/src/com/nusang/controller/assistance/ConAsist.java
@@ -21,6 +21,7 @@ public class ConAsist {
 	public static final String SERVLET_READPOST = "/post/readPost";
 	public static final String SERVLET_UPDATEPOST = "/post/updatePost";
 	public static final String SERVLET_DELETEPOST = "/post/deletePost";
+	public static final String SERVLET_MYINFODISTANCE = "/myinfo/distance";
 	
 	public static final String URL_CHKLOCATION = "/1_main/index.jsp";
 	public static final String URL_MAIN = "/1_main/main.jsp";
