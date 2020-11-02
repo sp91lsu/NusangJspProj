9b683ed15e35efc494944503c95898389ae1e3c9
Sunghoon-PC\Sunghoon <pppsh720@naver.com>
2020-11-02 18:20:50 +0900

    no message

--- NusangJspProject/src/com/nusang/action/main/MainAction.java
+++ NusangJspProject/src/com/nusang/action/main/MainAction.java
@@ -1,6 +1,11 @@
 package com.nusang.action.main;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.List;
@@ -34,52 +39,49 @@ public class MainAction implements Action {
 
 		// 근방의 게시글 가지고 오기
 		List<Post> postList = PostDao.getInstance().findPostByLocation(location, distance);
-		System.out.println("이 근처 " + distance + "km의 게시글 갯수 : "+ postList.size());
+		System.out.println("이 근처 " + distance + "km의 게시글 갯수 : " + postList.size());
 		request.setAttribute("postList", postList);
 
 		// 기본 업로드 경로 생성 및 이미지 업로드
 		String savePath = request.getSession().getServletContext().getRealPath("upload");
-		File mkFile = new File(savePath );
+		System.out.println("세이브패스 "+savePath);
+		File mkFile = new File(savePath);
 		if (!mkFile.exists()) {
 			// 디렉토리 생성 메서드
 			mkFile.mkdirs();
 			System.out.println("created directory successfully!");
-			
-			int maxPostSize = 5 * 1024 * 1024; // 최대 5M
-			String encoding = "utf-8"; // 인코딩
-			FileRenamePolicy policy = new DefaultFileRenamePolicy(); // 중복파일명에 대한 rename 정책
-			MultipartRequest multi = null;
-			ServletContext context = request.getServletContext();
-			String saveDirectory = context.getRealPath("upload");
-
-			// MultipartRequest 생성 단계에서 이미 파일은 저장됨.
-			try {
-				multi = new MultipartRequest(request, saveDirectory, maxPostSize, encoding, policy);
-			} catch (Exception e1) {
-				e1.printStackTrace();
-			}
-
-			// 2. 업로드된 파일의 '원본이름(들)' 과 '저장된 이름(들)' 받아오기
-			List<String> originalFileNames = new ArrayList<String>();
-			List<String> fileSystemNames = new ArrayList<String>();
-
-			Enumeration names = multi.getFileNames(); // type='file' 요소 name 들 추출
-
-			while (names.hasMoreElements()) {
-				String name = (String) names.nextElement();
-				String originalFileName = multi.getOriginalFileName(name);
-				String fileSystemName = multi.getFilesystemName(name);
-				System.out.println("첨부파일: " + originalFileName + "->" + fileSystemName);
-
-				if (originalFileName != null && fileSystemName != null) {
-					originalFileNames.add(originalFileName);
-					fileSystemNames.add(fileSystemName);
-
-				}
-			} // end while
-
+			File file = new File("/WebContent/img/profile.png");
+			File mFile = new File(savePath + "/profile.png");
+		
+			copyFile(file, mFile);
 		}
 		return actionForward;
+	}
+
+	public void copyFile(File file, File mfile) throws IOException {
+		System.out.println("파일이 어떻게되니 "+file);
+		System.out.println("파일이 어떻게되니222 "+mfile);
+	System.out.println("기본이미지 생성해야한다!!!");
+		InputStream inStream = null;
+		OutputStream outStream = null;
+
+		try {
+			inStream = new FileInputStream(file); // 원본파일
+			outStream = new FileOutputStream(mfile); // 이동시킬 위치
+
+			byte[] buffer = new byte[1024];
+
+			int length;
+
+			while ((length = inStream.read(buffer)) > 0) {
+				outStream.write(buffer, 0, length);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			inStream.close();
+			outStream.close();
+		}
 	}
 
 }
