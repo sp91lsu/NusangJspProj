b9bdbf302fad4d1942f9dc01f345a219af83803b
SunBi91 <sp91lsu@gmail.com>
2021-01-08 05:19:26 +0900

    단순 메시지 변경

--- NusangJspProject/WebContent/0_common/js/header.js
+++ NusangJspProject/WebContent/0_common/js/header.js
@@ -1,5 +1,5 @@
 function sendNum(number){
-	var result = confirm("검색 반경을 "+number+"km 로 설정하시겠습니까?");
+	var result = confirm("기본 검색반경을 "+number+"km 로 설정하시겠습니까?");
 	
 	if(result){
 		location.href='/myinfo/distance?num='+number;
